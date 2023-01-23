6ceb839845961190fc903fe42c553da88b826b2b
Your NameDonna <devgenid@gmail.com>
2023-01-23 20:04:01 +0200

    More functionality

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerControl : MonoBehaviour
 {
@@ -10,10 +11,29 @@ public class PlayerControl : MonoBehaviour
     float moveInputX, moveInputZ, moveInputY;
     public int Speed=0; //This public value can be accessible from the editor
 
+    public TextMeshProUGUI scoreText;
+    private int score;
+    private int cubePoints = 5; //5 points per cube
+
+    public GameObject winText;
+    public GameObject gameOver;
+    private int cubeNumber=12;
+
+    public Transform Level2Teleport;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>(); // Get the Component of type Rigidbody in rb
+
+        score = 0;
+        SetScoreText();
+
+        winText.SetActive(false); //Hide winText
+        gameOver.SetActive(false); //Hide gameOver
+
+        //cubeNumber = GameObject.FindGameObjectsWithTag("Pickups").Length;
+        cubeNumber = 12;
     }
 
     void Update() 
@@ -46,10 +66,30 @@ public class PlayerControl : MonoBehaviour
         if(other.tag=="Pickups") 
         {
             other.gameObject.SetActive(false);
+            score += cubePoints; //Increase score when touching a cube
+            SetScoreText();
         }
         if (other.tag == "Enemy")
         {
             gameObject.SetActive(false);
+            gameOver.SetActive(true);
+            Time.timeScale = 0; //Freeze game when we lose - normal game velocity is 1.
+
+        }
+        if(other.tag == "Teleport")
+        {
+            transform.position = Level2Teleport.position;
+        }
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Score " + score; //UI element TextMeshPro
+
+        if(score >= cubeNumber * cubePoints) //we have 12 cubes
+        {
+            winText.SetActive(true);
+            Time.timeScale = 0; //Freeze game when we win - normal game velocity is 1.
         }
     }
 }
